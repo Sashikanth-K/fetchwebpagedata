bd24e8c2d5fba926be34c97a40053e62df0e9071
Sashikanth-K <sashikanth5890@gmail.com>
2023-04-21 14:44:43 +0530

    Setting up the project config to use typescript and jest as the testing framework

---
+++ src/fetch.ts
@@ -0,0 +1,11 @@
+function fetch(url: String): any {
+  // ...
+
+  return {
+    assets: [],
+    links: [],
+  };
+}
+
+
+export default fetch;
---
+++ tests/fetch.spec.ts
@@ -0,0 +1,12 @@
+import  fetch  from "../src/index";
+import { describe, test, expect } from "@jest/globals";
+
+describe("sum", () => {
+  test("must return an object", () => {
+    expect(fetch("https://www.google.com")).toEqual({
+      assets: [],
+      links: [],
+    });
+  });
+  
+});
